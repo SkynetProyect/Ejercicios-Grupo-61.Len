14981d48e3c4ad59469f3569be9cb63d60c17fb1
SkynetProject <o.lamprea99gmail.com>
2023-08-01 13:15:56 -0500

    Correccion ortografica.

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 
 def entrada_datos(nombre=input(" Ingrese Nombre: "), edad=input(" Ingrese edad: ")):
-    print("Los datos ingresados son, Nombre "+nombre+" Edad "+edad)
+    print("Los datos ingresados son.  Nombre: "+nombre+", Edad: "+edad)
 
 
 entrada_datos()
